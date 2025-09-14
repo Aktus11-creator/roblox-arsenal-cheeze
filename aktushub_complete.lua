c680b04dc46d3aedc35a2be64296682eff446c4b
Aktus11-creator <adam.novotny.cb@gmail.com>
2025-09-14 11:10:14 +0200

    Update aktushub_complete.lua

--- aktushub_complete.lua
+++ aktushub_complete.lua
@@ -31,7 +31,7 @@ game:GetService("RunService").Heartbeat:connect(function(step)if flying and c an
 
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bitef4/Recode/main/UI/Kavo_1.lua"))()
-local Window = Library.CreateLib("CHEATER.FUN | Arsenal | v1.6 ", "BlueTheme")
+local Window = Library.CreateLib("AktusHUB | Arsenal | v1.7 ", "BlueTheme")
 
 local Welcome = Window:NewTab("Main")
 local MainSection = Welcome:NewSection("Welcome To AdvanceTech | " .. game.Players.LocalPlayer.Name)
